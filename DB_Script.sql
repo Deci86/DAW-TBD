3f4bf0c0cebf8bf15fb914bf62d7aba5faaf7d32
Cristhian Jesús Ricardo Nicho Bruno <cristhiannicho61063070@gmail.com>
2026-06-25 17:21:48 -0500

    Modify table structure for Clientes and Ofertas

--- DB_Script.sql
+++ DB_Script.sql
@@ -7,19 +7,20 @@ CREATE TABLE Clientes (
     id INT IDENTITY(1,1) PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-	pass_hash VARCHAR(255) NOT NULL,
-	promedio_calificacion_comprador DECIMAL(3,2) DEFAULT 0.00,
-	calificacion_vendedor DECIMAL(3,2) DEFAULT 0.00,
+    pass_hash VARCHAR(255) NOT NULL,
+    promedio_calificacion_comprador DECIMAL(3,2) DEFAULT 0.00,
+    calificacion_vendedor DECIMAL(3,2) DEFAULT 0.00,
     fecha_registro DATETIME2 DEFAULT GETDATE()
 );
 
 CREATE TABLE Ofertas (
     id INT IDENTITY(1,1) PRIMARY KEY,
     cliente_id INT,
-    moneda_a_enviar VARCHAR(150) NOT NULL,
-	moneda_a_recibir VARCHAR(150) NOT NULL,
-	tipo_cambio DECIMAL(3,2) NOT NULL,
-	estado BIT DEFAULT 1,
+    moneda_a_enviar VARCHAR(10) NOT NULL,
+    moneda_a_recibir VARCHAR(10) NOT NULL,
+    cantidad DECIMAL(18,2) NOT NULL,
+    tipo_cambio DECIMAL(10,4) NOT NULL,
+    estado BIT DEFAULT 1,
     fecha_creacion DATETIME2 DEFAULT GETDATE(),
     CONSTRAINT FK_Ofertas_Clientes FOREIGN KEY (cliente_id) 
         REFERENCES Clientes(id) ON DELETE CASCADE
